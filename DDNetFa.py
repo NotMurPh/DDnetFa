d90d131216cd4af83a5ebc932310b27a8e57fd42
PouyaCr72 <Pouyannpouya@gmail.com>
2020-10-21 16:05:57 +0330

    Files Created.

---
+++ DDNetFa.py
@@ -0,0 +1,117 @@
+import pyperclip
+class FarsiConverter():
+
+    lastchar=None
+    ConvertedText=''
+    UnknwonChars=''
+    Farsi=False
+
+    def __init__(self,name,isolated,start,mid,end):
+        self.name=name
+        self.isolated=isolated
+        self.start=start
+        self.mid=mid
+        self.end=end
+        Characters.append(self)
+
+    def BindWord(self):
+        #agar char ghabli nadashtim ya char ghabli nemichasbid be badi ya char jadid nemichasbe be ghabli : isole bezar
+        if FarsiConverter.lastchar == None or FarsiConverter.lastchar.mid == FarsiConverter.lastchar.end or self.start==self.mid:
+            FarsiConverter.ConvertedText+=self.isolated
+
+        #agar hatman lastchar dashtim = vared sho
+        if FarsiConverter.lastchar!=None:
+            #agar char jadid be ghabli michasbid va char ghabli end bod : char ghabli mishe mid char jadid end
+            if self.start!=self.mid and FarsiConverter.ConvertedText[-1]==FarsiConverter.lastchar.end:
+                FarsiConverter.ConvertedText=FarsiConverter.ConvertedText[:-1]+FarsiConverter.lastchar.mid+self.end
+            #agar char jadid be ghabli michasbid va char ghabli isole bode va be jadid michasbe : char ghabli start she jadid end she
+            if self.start!=self.mid and FarsiConverter.ConvertedText[-1]==FarsiConverter.lastchar.isolated and FarsiConverter.lastchar.mid!=FarsiConverter.lastchar.end:
+                FarsiConverter.ConvertedText=FarsiConverter.ConvertedText[:-1]+FarsiConverter.lastchar.start+self.end
+
+        FarsiConverter.lastchar=self
+
+    @staticmethod
+    def AddUnknownChars():
+        if len(FarsiConverter.UnknwonChars)>0:
+            if len(FarsiConverter.UnknwonChars)>1:
+                for LastLeftNonChar in range(len(FarsiConverter.UnknwonChars)):
+                    if FarsiConverter.UnknwonChars[LastLeftNonChar].isalpha()==True:
+                        break
+                # LastLeftNonChar-=1
+                for LastRightNonChar in range(len(FarsiConverter.UnknwonChars)-1,0,-1):
+                    if FarsiConverter.UnknwonChars[LastRightNonChar].isalpha()==True:
+                        break
+                LastRightNonChar+=1
+
+                if FarsiConverter.Farsi==True:
+                    FarsiConverter.UnknwonChars=FarsiConverter.UnknwonChars[LastRightNonChar:][::-1]+FarsiConverter.UnknwonChars[LastLeftNonChar:LastRightNonChar]+FarsiConverter.UnknwonChars[:LastLeftNonChar][::-1]
+                    
+            FarsiConverter.ConvertedText+=FarsiConverter.UnknwonChars[::-1]
+            FarsiConverter.UnknwonChars=''
+            FarsiConverter.lastchar=None
+
+    @staticmethod
+    def ReverseAndCopy():
+        pyperclip.copy(FarsiConverter.ConvertedText[::-1])
+        if __name__ == '__main__':
+            print('\n'+FarsiConverter.ConvertedText[::-1]+'\n')
+            Manager()
+
+Characters=[]
+A1=FarsiConverter(name='ا',isolated='ﺍ',start='ﺍ',mid='ﺎ',end='ﺎ')
+B=FarsiConverter(name='ب',isolated='ﺏ',start='ﺑ',mid='ﺒ',end='ﺐ')
+P=FarsiConverter(name='پ',isolated='ﭖ',start='ﭘ',mid='ﭙ',end='ﭗ')
+T1=FarsiConverter(name='ت',isolated='ﺕ',start='ﺗ',mid='ﺘ',end='ﺖ')
+S1=FarsiConverter(name='ث',isolated='ﺙ',start='ﺛ',mid='ﺜ',end='ﺚ')
+J=FarsiConverter(name='ج',isolated='ﺝ',start='ﺟ',mid='ﺠ',end='ﺞ')
+Ch=FarsiConverter(name='چ',isolated='ﭺ',start='ﭼ',mid='ﭽ',end='ﭻ')
+H1=FarsiConverter(name='ح',isolated='ﺡ',start='ﺣ',mid='ﺤ',end='ﺢ')
+Kh=FarsiConverter(name='خ',isolated='ﺥ',start='ﺧ',mid='ﺨ',end='ﺦ')
+D=FarsiConverter(name='د',isolated='ﺩ',start='ﺩ',mid='ﺪ',end='ﺪ')
+Z1=FarsiConverter(name='ذ',isolated='ﺫ',start='ﺫ',mid='ﺬ',end='ﺬ')
+R=FarsiConverter(name='ر',isolated='ﺭ',start='ﺭ',mid='ﺮ',end='ﺮ')
+Z2=FarsiConverter(name='ز',isolated='ﺯ',start='ﺯ',mid='ﺰ',end='ﺰ')
+Jh=FarsiConverter(name='ژ',isolated='ﮊ',start='ﮊ',mid='ﮋ',end='ﮋ')
+S2=FarsiConverter(name='س',isolated='ﺱ',start='ﺳ',mid='ﺴ',end='ﺲ')
+Sh=FarsiConverter(name='ش',isolated='ﺵ',start='ﺷ',mid='ﺸ',end='ﺶ')
+S3=FarsiConverter(name='ص',isolated='ﺹ',start='ﺻ',mid='ﺼ',end='ﺺ')
+Z3=FarsiConverter(name='ض',isolated='ﺽ',start='ﺿ',mid='ﻀ',end='ﺾ')
+T2=FarsiConverter(name='ط',isolated='ﻁ',start='ﻃ',mid='ﻄ',end='ﻂ')
+Z4=FarsiConverter(name='ظ',isolated='ﻅ',start='ﻇ',mid='ﻈ',end='ﻆ')
+A2=FarsiConverter(name='ع',isolated='ﻉ',start='ﻋ',mid='ﻌ',end='ﻊ')
+Gh1=FarsiConverter(name='غ',isolated='ﻍ',start='ﻏ',mid='ﻐ',end='ﻎ')
+F=FarsiConverter(name='ف',isolated='ﻑ',start='ﻓ',mid='ﻔ',end='ﻒ')
+Gh2=FarsiConverter(name='ق',isolated='ﻕ',start='ﻗ',mid='ﻘ',end='ﻖ')
+K=FarsiConverter(name='ک',isolated='ﮎ',start='ﮐ',mid='ﮑ',end='ﮏ')
+G=FarsiConverter(name='گ',isolated='ﮒ',start='ﮔ',mid='ﮕ',end='ﮓ')
+L=FarsiConverter(name='ل',isolated='ﻝ',start='ﻟ',mid='ﻠ',end='ﻞ')
+M=FarsiConverter(name='م',isolated='ﻡ',start='ﻣ',mid='ﻤ',end='ﻢ')
+N=FarsiConverter(name='ن',isolated='ﻥ',start='ﻧ',mid='ﻨ',end='ﻦ')
+V=FarsiConverter(name='و',isolated='ﻭ',start='ﻭ',mid='ﻮ',end='ﻮ')
+H2=FarsiConverter(name='ه',isolated='ﻩ',start='ﻫ',mid='ﻬ',end='ﻪ')
+Y=FarsiConverter(name='ی',isolated='ﻯ',start='ﻳ',mid='ﻴ',end='ﻰ')
+
+def Manager(input_text=None):
+    FarsiConverter.lastchar=None
+    FarsiConverter.ConvertedText=''
+    FarsiConverter.UnknwonChars=''
+    FarsiConverter.Farsi=False
+
+    if __name__ == '__main__':
+        input_text=input('Enter Your Persian Text : ')
+
+    for each_char in input_text:
+        for each_object in Characters:
+            if each_char == each_object.name:
+                FarsiConverter.Farsi=True
+                FarsiConverter.AddUnknownChars()
+                each_object.BindWord()
+                break
+        else:
+            FarsiConverter.UnknwonChars+=each_char
+    else:
+        FarsiConverter.AddUnknownChars()
+        FarsiConverter.ReverseAndCopy()
+
+if __name__ == '__main__':
+    Manager()
---
+++ DDNetFaGUI.py
@@ -0,0 +1,178 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow,QApplication
+from PyQt5.QtCore import QPoint
+import DDNetFa
+import random
+
+
+class Ui_MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setObjectName('MainWindow')
+        self.setFixedSize(800, 220)
+
+        self.setStyleSheet('''
+        *:focus{
+            outline: none;
+        }
+
+        QMainWindow{
+            background-color: #222831;
+            border:none;
+        }
+
+        #title_text{
+            color: #eeeeee;
+        }
+
+        #exit_btn, #min_btn{
+            color: white;
+            border: none;
+        }
+
+        #min_btn:hover{
+            background-color: #34343E;
+        }
+
+        #exit_btn:hover{
+            background-color:#F20530;
+        }
+
+        #input_text{
+            background-color: transparent;
+            border: none;
+            border-bottom: 2px solid #393e46;
+            color: #eeeeee;
+            padding-bottom: 5px;
+        }
+        
+        #copy_btn{
+            border: 2px solid #393e46;
+            border-radius: 15px;
+            background-color: transparent;
+            color: #eeeeee;
+        }
+
+        #contact_text{
+            color: #eeeeee;
+        }''')
+
+        font = QtGui.QFont()
+        font.setFamily('IRANSansWeb')
+        self.setFont(font)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate('MainWindow', 'DDNetFa'))
+        self.setWindowIcon(QtGui.QIcon(r'C:\Users\PouyaCr72\Desktop\Projects\Python Projects\DDnetFarsi\GUI\Icons\DDNetFaIcon.svg'))
+        self.show()
+        
+        self.exit_btn = QtWidgets.QPushButton(self)
+        self.exit_btn.setGeometry(QtCore.QRect(770,0,30,30))
+        self.exit_btn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.exit_btn.setObjectName('exit_btn')
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(r'C:\Users\PouyaCr72\Desktop\Projects\Python Projects\DDnetFarsi\GUI\Icons\close.svg'))
+        self.exit_btn.setIcon(icon)
+        self.exit_btn.clicked.connect(self.close)
+        self.exit_btn.show()
+
+        self.min_btn = QtWidgets.QPushButton(self)
+        self.min_btn.setGeometry(QtCore.QRect(740,0,30,30))
+        self.min_btn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.min_btn.setObjectName('min_btn')
+        icon.addPixmap(QtGui.QPixmap(r'C:\Users\PouyaCr72\Desktop\Projects\Python Projects\DDnetFarsi\GUI\Icons\min.svg'))
+        self.min_btn.setIcon(icon)
+        self.min_btn.clicked.connect(self.showMinimized)
+        self.min_btn.show()
+        
+        self.title_text = QtWidgets.QLabel(self)
+        self.title_text.setGeometry(QtCore.QRect(12,12,393,20))
+        self.title_text.setObjectName('title_text')
+        font.setPointSize(8)
+        self.title_text.setFont(font)
+        self.title_text.setText(_translate('MainWindow', 'DDNetFa ~ Made by OmeGa'))
+        self.title_text.show()
+
+        self.input_text = QtWidgets.QLineEdit(self)
+        self.input_text.setGeometry(QtCore.QRect(50, 95, 700, 30))
+        font.setPointSize(10)
+        self.input_text.setFont(font)
+        self.input_text.setObjectName('input_text')
+        self.input_text.setPlaceholderText(_translate('MainWindow', 'متن مورد نظر...'))
+        self.input_text.textChanged.connect(self.ChangeBtnColor)
+        self.input_text.show()
+
+        self.copy_btn = QtWidgets.QPushButton(self)
+        self.copy_btn.setGeometry(QtCore.QRect(315, 147, 170, 30))
+        font.setPointSize(12)
+        self.copy_btn.setFont(font)
+        self.copy_btn.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.copy_btn.setObjectName('copy_btn')
+        self.copy_btn.setText(_translate('MainWindow', 'کپی کن'))
+        self.copy_btn.clicked.connect(self.CopyBtn)
+        self.copy_btn.show()
+
+        self.output_text = QtWidgets.QLabel(self)
+        self.output_text.setGeometry(QtCore.QRect(630, 130, 120, 20))
+        self.output_text.setObjectName('output_text')
+        self.output_text.setText(_translate('MainWindow', 'متن شما با موفقت کپی شد'))
+        
+        down=-4
+        right=15
+        self.contact_text = QtWidgets.QLabel(self)
+        self.contact_text.setGeometry(QtCore.QRect(264+right,196+down,272,20))
+        self.contact_text.setObjectName('contact_text')
+        font.setPointSize(8)
+        self.contact_text.setFont(font)
+        self.contact_text.setText(_translate('MainWindow', 'Z3Neuga        Omegatee.ir:8302        @PouyaCr72'))
+        self.contact_text.show()
+
+        self.discord_icon = QtWidgets.QLabel(self)
+        self.discord_icon.setGeometry(QtCore.QRect(242+right,195+down,21,21))
+        self.discord_icon.setObjectName('discord_icon')
+        self.discord_icon.setPixmap(QtGui.QPixmap(r'C:\Users\PouyaCr72\Desktop\Projects\Python Projects\DDnetFarsi\GUI\Icons\Discord-Logo-White.svg'))
+        self.discord_icon.setScaledContents(True)
+        self.discord_icon.show()
+
+        self.telegram_icon = QtWidgets.QLabel(self)
+        self.telegram_icon.setGeometry(QtCore.QRect(435+right,197+down,15,15))
+        self.telegram_icon.setObjectName('telegram_icon')
+        self.telegram_icon.setPixmap(QtGui.QPixmap(r'C:\Users\PouyaCr72\Desktop\Projects\Python Projects\DDnetFarsi\GUI\Icons\telegram.svg'))
+        self.telegram_icon.setScaledContents(True)
+        self.telegram_icon.show()
+
+        self.teeworlds_icon = QtWidgets.QLabel(self)
+        self.teeworlds_icon.setGeometry(QtCore.QRect(316+right,196+down,17,17))
+        self.teeworlds_icon.setObjectName('teeworlds_icon')
+        self.teeworlds_icon.setPixmap(QtGui.QPixmap(r'C:\Users\PouyaCr72\Desktop\Projects\Python Projects\DDnetFarsi\GUI\Icons\teeworlds.svg'))
+        self.teeworlds_icon.setScaledContents(True)
+        self.teeworlds_icon.show()
+
+        self.oldPos = self.pos()
+
+    def ChangeBtnColor(self):
+        if self.input_text.text() !='':
+            self.copy_btn.setStyleSheet('border: 2px solid #d65a31;')
+        else:
+            self.copy_btn.setStyleSheet('border: 2px solid #393e46;')
+            
+    def CopyBtn(self):
+        if self.input_text.text()!='':
+            DDNetFa.Manager(self.input_text.text())
+            self.input_text.setText('')
+            self.output_text.show()
+            self.output_text.setStyleSheet(f'color:rgb({random.randint(0,255)},{random.randint(0,255)},{random.randint(0,255)});font-family:IRANSansWeb;')
+
+    def mousePressEvent(self, event):
+        self.oldPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        delta = QPoint (event.globalPos() - self.oldPos)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldPos = event.globalPos()
+        
+if __name__=='__main__':
+    import sys
+    app = QApplication(sys.argv)
+    ui = Ui_MainWindow()
+    sys.exit(app.exec_())
